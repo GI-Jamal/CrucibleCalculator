9fda57e575b5cdc554a1c0b0a1495cc01b35e72b
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-12 16:00:27 -0400

    commit msg

--- js/site.js
+++ js/site.js
@@ -10,9 +10,10 @@ function add(num1, num2)
   {
     Swal.fire({
       icon: "error",
+      heightAuto: false,
       title: "Oops!",
       text: "Please enter valid numbers for the start and end values",
-      backdrop: "false",
+      backdrop: "false"
     });
 
   } 
@@ -22,6 +23,81 @@ function add(num1, num2)
 
     let results = document.getElementById("results");
 
-    results.innerHTML = number3;
+    results.innerText = number3;
+  }
+}
+
+// TODO: subtract
+function subtract(num1, num2) {
+  let value1 = document.getElementById("firstNumber").value;
+  let value2 = document.getElementById("secondNumber").value;
+
+  let number1 = parseFloat(value1);
+  let number2 = parseFloat(value2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops!",
+      text: "Please enter valid numbers for the start and end values",
+      heightAuto: false,
+      backdrop: "false",
+    });
+  } else {
+    let number3 = number1 - number2;
+
+    let results = document.getElementById("results");
+
+    results.innerText = number3;
+  }
+}
+
+// TODO: multiply
+function multiply(num1, num2) {
+  let value1 = document.getElementById("firstNumber").value;
+  let value2 = document.getElementById("secondNumber").value;
+
+  let number1 = parseFloat(value1);
+  let number2 = parseFloat(value2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    Swal.fire({
+      icon: "error",
+      heightAuto: false,
+      title: "Oops!",
+      text: "Please enter valid numbers for the start and end values",
+      backdrop: "false",
+    });
+  } else {
+    let number3 = number1 * number2;
+
+    let results = document.getElementById("results");
+
+    results.innerText = number3;
+  }
+}
+
+// TODO: divide
+function divide(num1, num2) {
+  let value1 = document.getElementById("firstNumber").value;
+  let value2 = document.getElementById("secondNumber").value;
+
+  let number1 = parseFloat(value1);
+  let number2 = parseFloat(value2);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    Swal.fire({
+      icon: "error",
+      heightAuto: false,
+      title: "Oops!",
+      text: "Please enter valid numbers for the start and end values",
+      backdrop: "false",
+    });
+  } else {
+    let number3 = number1 / number2;
+
+    let results = document.getElementById("results");
+
+    results.innerText = number3;
   }
 }
